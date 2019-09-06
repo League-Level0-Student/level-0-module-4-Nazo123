913e00a59f83b3c71a3ba335b5cb6fdff27413ea
League Student <league@iMac-18.attlocal.net>
2019-09-05 18:29:55 -0700

    Pi_aloud,happy_pet
    :smile:

--- src/_09_pi_aloud/PiAloud.java
+++ src/_09_pi_aloud/PiAloud.java
@@ -9,20 +9,39 @@ import java.util.Scanner;
 public class PiAloud {
 
 	// 1. Make a main method and make sure your program can run
+public static void main(String[] args) {
+	
 
 	// 2. Make a String variable to hold the value of Pi. You could use http://www.piday.org/million/ for the value.
-
+String Pi = "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679821480865132823066470938446095505822317";
 	// 3. Print out some digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)"
-
+for (int i=0;i<10;i++) {
+	
+	System.out.print((Pi.charAt(i)));
+	
+}
 	// 4. Print ALL the digits of of Pi (hint: use a loop)
-
+//for (int o=0;o<141;o++) {
+	
+	//speak((Pi.charAt(o)));
+	
+//}
 	// 5. Use the speak() method to speak all the digits of Pi.
-
+System.out.println("   Guess the next number of pi");
 	// [ADVANCED]
-	// *6. Get a character from the user using the getInputFromUser() method
+if	(getInputFromUser()==(Pi.charAt(10))){
+	System.out.println("Correct");
+}
+else {
+	System.out.println("Wrong");
+}
 	// *7. Compare the users' char to the next digit of Pi
 	// *8. If they are correct, print out "correct". If they are not, print "incorrect" to System.err.println
-
+}
+	private static void print(char charAt) {
+		// TODO Auto-generated method stub
+		
+	}
 	static void speak(char characterToSpeak) {
 		try {
 			Runtime.getRuntime().exec("say " + characterToSpeak).waitFor();
@@ -44,7 +63,7 @@ public class PiAloud {
 		Scanner scanner = new Scanner(System.in);
 		return scanner.next().toCharArray()[0];
 	}
-
 }
+
 
 
--- src/_10_happy_pet/HappyPet.java
+++ src/_10_happy_pet/HappyPet.java
@@ -2,29 +2,67 @@ package _10_happy_pet;
 import javax.swing.JOptionPane;
 
 public class HappyPet {
-	
+	static String pet;
+	static int task;
 	// 2. Add the following variable to the next line: static int happinessLevel = 0;
 	// this will be used to store the happiness of your pet
-	
+	static int happinessLevel = 0;
 	public static void main(String[] args) {
 		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
-
+		String pet = JOptionPane.showInputDialog("What pet do you want to buy? A dog or a cat?");
 		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
 		
+for (int i=0;1<4;i++) {
+
 			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
-			int task = JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Button1", "Button2", "Button3" }, null);
+			task = JOptionPane.showOptionDialog(null, "What do you want to do to your pet to make it happier?", "1", 0, JOptionPane.INFORMATION_MESSAGE, null,
+					new String[] { "Give It Food", "Take It On A Walk", "Groom It" }, null);
 
 			// 5. Use user input to call the appropriate method created in step 4.
-
+petResponse();
 			// 6. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
-
+if (happinessLevel>59) {
+	break;
+}
 	}
-
+	}
 	// 4. Create methods to handle each of your user selections.
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
 	//    and INCREMENT the pet's happiness Level.
+	public static void petResponse(){
+		if (task==1) {
+			if(pet.equalsIgnoreCase("Dog")) {
+				 happinessLevel =+25;
+				JOptionPane.showMessageDialog(null, "Dog loves walking!");
+			}
+			else {
+			happinessLevel =+10;
+			JOptionPane.showMessageDialog(null, "Cat likes walking");
+			}
+		}
+		if (task==0) {
+			if(pet.equalsIgnoreCase("Cat")) {
+				happinessLevel =+25;
+				JOptionPane.showMessageDialog(null, "Cat loves eating!");
+			}
+			else {
+			happinessLevel =+10;
+			JOptionPane.showMessageDialog(null, "Dog likes eating");
+			}
+		}
+		if (task==3) {
+			if(pet.equalsIgnoreCase("Dog")) {
+				happinessLevel =+10;
+				JOptionPane.showMessageDialog(null, "Dog likes being groomed!");
+			}
+			else {
+			 happinessLevel =+10;
+			JOptionPane.showMessageDialog(null, "Cat likes being groomed");
+			}
+		}
+		
+	}
 }
